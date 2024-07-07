6c471da79bc674696e3763428c709b732f760c95
“https://github.com/Ahaola” <1360099242@qq.com>
2024-07-07 15:53:21 +0800

    Python时间模块time的使用.

---
+++ DreamPy/datetime.py
@@ -0,0 +1,22 @@
+import calendar
+
+import datetime
+import time
+# timestamp = time.time()
+# print(timestamp)
+
+# time.sleep(3)
+# timestamp = time.time()
+# local_time = time.localtime(timestamp)
+# print(local_time)
+# formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+# print(formatted_time)
+
+#获取当前时间
+# now = datetime.now()
+# futtle_time = now + datetime.timedelta(days=3)
+# print(now)
+# print(futtle_time)
+
+cal = calendar.month(2024, 7)
+print(cal)
